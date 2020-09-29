940703c06faee4de25fbcf420ccef9031736410d
Dicardo Xue <49444151+DicardoX@users.noreply.github.com>
2020-09-29 19:41:41 +0800

    Update 0145_二叉树的后序遍历.cpp

--- Normal/0145_二叉树的后序遍历.cpp
+++ Normal/0145_二叉树的后序遍历.cpp
@@ -34,6 +34,32 @@ public:
 
 /** 方法二：迭代 **/
 
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ret;
+        if(!root) return ret;
+        stack<TreeNode*> myStack1;        // myStack1用来第一次存储
+        stack<TreeNode*> myStack2;        // myStack2用来第二次存储
 
+        myStack1.push(root);
+        while (!myStack1.empty() || !myStack2.empty()) {
+            if(myStack1.empty()) {        // 若myStack1已经空，则将myStack2里的元素依次pop
+                while(!myStack2.empty()) {
+                    int topVal = myStack2.top()->val;
+                    myStack2.pop();
+                    ret.emplace_back(topVal);
+                }
+                break;
+            }
+            TreeNode* curNode = myStack1.top();     // 获取myStack1的栈顶元素，弹出
+            myStack1.pop();
+            if(curNode->left) myStack1.push(curNode->left);       // 先看左子树，因为在myStack2输出时会再次反序
+            if(curNode->right) myStack1.push(curNode->right);     // 再看右子树
+            myStack2.push(curNode);                 // 将当前节点push进myStack2
+        }
+        return ret;
+    }
+};
 
 
