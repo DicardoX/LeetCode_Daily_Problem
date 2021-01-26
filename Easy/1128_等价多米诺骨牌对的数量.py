12b74a53ea75229bbdcd4521dbc6ee5976709593
Dicardo Xue <49444151+DicardoX@users.noreply.github.com>
2021-01-26 21:19:07 +0800

    Update 1128_等价多米诺骨牌对的数量.py

--- Easy/1128_等价多米诺骨牌对的数量.py
+++ Easy/1128_等价多米诺骨牌对的数量.py
@@ -1,1 +1,12 @@
+# 题目见：https://leetcode-cn.com/problems/number-of-equivalent-domino-pairs/
 
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        matrix = [0] * 100
+        ret = 0
+
+        for x, y in dominoes:
+            index = (x * 10 + y if x <= y else y * 10 + x)
+            ret += matrix[index]
+            matrix[index] += 1
+        return ret
